69761d3950ff6217a5cc0f1bb2d7e0cd70c547f0
CadoFox <157754423+CadoFox@users.noreply.github.com>
2024-03-07 15:03:46 -0500

    yep

---
+++ 05Functions/March7.py
@@ -0,0 +1,23 @@
+number = 10
+base = 2
+exponent = 3
+
+def circle_area(radius):
+    return radius * radius * 3.14
+
+def square_area(sidelength):
+    return sidelength * sidelength
+
+def sqr(x, n):
+    if x < n:
+        return x ** n
+    
+def sumup(n):
+    sum = 0
+    for i in range(n + 1):
+        sum += i
+    return sum
+
+def getInstruments():
+    return 'Drum', 'Guitar', 'Bass'
+
